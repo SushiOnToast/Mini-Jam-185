2e618f1f433a43d5b13f48646f1df99fbae6942a
Sushi <sushbomm@gmail.com>
2025-05-23 12:51:13 +0530

    set up main room (Corridor)

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,8 +3,10 @@ extends Node2D
 @onready var state_manager: StateManager = $StateManager
 
 func _ready() -> void:
-	state_manager.show_menu()
+	state_manager.initialize()
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
-		state_manager.show_pause()
+	#if Input.is_action_just_pressed("pause"):
+		#state_manager.show_pause()
+	pass
+	
