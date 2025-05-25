0fc4d51042eb73d37623e2075f6ec2dd808ab969
shikha <shikhaandala@gmail.com>
2025-05-25 22:00:50 +0530

    yuh

--- scripts/tp_zone.gd
+++ scripts/tp_zone.gd
@@ -1,25 +1,21 @@
 extends Area2D
 
-@export var teleport_location: NodePath
-var can_teleport := false
-var player
+@export var target_scene_path: String = "res://ScienceRoom.tscn"
+
+var player_in_zone := false
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
 
+func _process(delta):
+	if player_in_zone and Input.is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file(target_scene_path)
+
 func _on_body_entered(body):
-	if body.name == "player": # Replace with your player's node name
-		can_teleport = true
-		player = body
+	if body.name == "Player": # adjust to match your player node name
+		player_in_zone = true
 
 func _on_body_exited(body):
-	if body.name == "player":
-		can_teleport = false
-		player = null
-
-func _process(delta):
-	if can_teleport and Input.is_action_just_pressed("interact"):
-		if teleport_location and player:
-			var destination = get_node(teleport_location)
-			player.global_position = destination.global_position
+	if body.name == "Player":
+		player_in_zone = false
