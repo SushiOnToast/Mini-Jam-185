f000f7b6f078061db3fa91d8c7cffb51cf3e3e14
Sushi <sushbomm@gmail.com>
2025-05-25 11:55:51 +0530

    detect when new day started

--- scripts/UI/day_night_panel.gd
+++ scripts/UI/day_night_panel.gd
@@ -3,13 +3,16 @@ extends Control
 @onready var day_label: Label = $DayCounter/DayLabel
 @onready var time_label: Label = $Time/TimeLabel
 
-@export var speed: int = 10
-
+@export var speed: int = 100
 
 func _ready() -> void:
 	DayAndNightCycleManager.time_tick.connect(on_time_tick)
+	DayAndNightCycleManager.time_tick_day.connect(on_new_day)  # ← connect day change
 	DayAndNightCycleManager.game_speed = speed
-	
+
 func on_time_tick(day: int, hour: int, minute: int) -> void:
-	day_label.text = "Day " + str(day)
 	time_label.text = "%02d:%02d" % [hour, minute]
+
+func on_new_day(day: int) -> void:
+	day_label.text = "Day " + str(day)
+	print("🌅 A new day has started: Day ", day)
